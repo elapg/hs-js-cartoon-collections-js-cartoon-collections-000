478992f5b36f2612e8375f5e047d769b86ac201a
Mariela Gonzales <mpgonzales14@gmail.com>
2019-05-01 03:59:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
+
 function dwarfRollCall(dwarves) {
+var numberedDwarves = "";
+ for (var i= 0; i < dwarves.length; i++){ 
+   numberedDwarves += `${i+1}. ${dwarves[i]} `;
+ }
+ return numberedDwarves;
+ }
+ 
+ var planeteerCalls= ["earth", "wind", "fire","water", "heart"]
+ 
+ function summonCaptainPlanet(planeteerCalls) {
+   var numberedplanteerCalls = "";
+for (var i = 0; i < planeteerCalls.length; i++){
+  var toBeChanged = planeteerCalls[i];
+  toBeChanged = toBeChanged.toUpperCase();
+  toBeChanged +="!"
+  planeteerCalls[i] = toBeChanged
+}
+return planeteerCalls;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+console.log(summonCaptainPlanet(planeteerCalls));
+
+
+function longPlaneteerCalls(short_words){
+
+var longerThanFour = false;
+for (var i = 0; i < short_words.length; i++){
+  if (short_words[i].length > 4) {
+    longerThanFour = true;
+  }    
+}
+return longerThanFour;
 }
 
-function longPlaneteerCalls(words) {
+function findTheCheese(snacks) {
+ var resultOfCheeseSearch = "no cheese!";
+  var possibleCheeses = ["cheddar", "gouda", "camembert"];
+  for (var i= 0; i < snacks.length; i++) {
+  
+    if  (possibleCheeses.includes(snacks[i])) {
+    resultOfCheeseSearch = snacks[i];
+}
+}
+ return resultOfCheeseSearch;
 }
 
-function findTheCheese (foods) {
-}
+    
