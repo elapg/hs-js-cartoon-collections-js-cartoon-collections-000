0b196195fa8de7af619d3582218b08d0719bc53d
Mariela Gonzales <mpgonzales14@gmail.com>
2019-06-03 21:21:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,16 +33,15 @@ for (var i = 0; i < short_words.length; i++){
 return longerThanFour;
 }
 
-function findTheCheese(snacks) { // ["grapes", "cheddar", "gouda"]
+function findTheCheese(snacks) { 
   var resultOfCheeseSearch = "no cheese!";
   var possibleCheeses = ["cheddar", "gouda", "camembert","swiss"];
   for (var i= 0; i < snacks.length; i++) { 
     if  (possibleCheeses.includes(snacks[i])) {
-      return snacks[i]; // cheddar
+      return snacks[i]; 
     }
   }
-  return resultOfCheeseSearch;
-}
+
 
 
   
